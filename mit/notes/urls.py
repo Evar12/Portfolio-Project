c3752572b16a773030f7b2592b4dd3853fb01632
Evar12 <evaristus12@gmail.com>
2024-09-19 17:56:36 +0100

    customized template files

--- mit/notes/urls.py
+++ mit/notes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()),
+    path('notes/', views.NotesListView.as_view(), name='my-notes'),
+    path('notes/<int:pk>', views.NotesDetailView.as_view() , name='my-notes-detail'),
+    path('notes/new', views.NotesCreateView.as_view(), name='new-notes'),
 ]
--- mit/notes/views.py
+++ mit/notes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Notes
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 
 # Create your views here.
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text',]
+    success_url = 'mit/notes/'
+    template_name = 'notes/notes_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 class NotesListView(ListView):
     model = Notes
     context_object_name = 'notes'
